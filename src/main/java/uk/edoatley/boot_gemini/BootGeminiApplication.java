47f244ffc6974b3256cbc33451ab55f3ad23b914
edoatley <edoatley@gmail.com>
2025-03-09 10:47:09 +0000

    Initial Project Setup

---
+++ src/main/java/uk/edoatley/boot_gemini/BootGeminiApplication.java
@@ -0,0 +1,13 @@
+package uk.edoatley.boot_gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGeminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/edoatley/boot_gemini/BootGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.edoatley.boot_gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
